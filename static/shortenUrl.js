443b5f9cc5cda729282b5c3e659100859363ed5f
Md Aminul Islam <95992822+mdaminul1024@users.noreply.github.com>
2023-09-20 14:26:44 +0600

    spinner add.js

--- static/shortenUrl.js
+++ static/shortenUrl.js
@@ -6,55 +6,78 @@ let getShortener = document.getElementById('show-link');
 let qrCodeShow = document.getElementById('QR-code-show');
 
 inputBtn.addEventListener('click', function() {
-    let validUrl = myInput.value;
-    const loadData = () => {
-        const url = `https://api.shrtco.de/v2/shorten?url=${validUrl}`;
 
-        console.log(url);
-        if (validUrl != '') {
-            fetch(url)
-                .then(res => res.json())
-                .then(data => displayShortUrl(data.result))
-                .catch((error) => {
-                    console.log(error);
-                    alert("Something Wrong! Please Input Valid URL. Thank You.")
-                })
-        } else {
-            alert("Something Wrong! Please Input Valid URL. Thank You.")
+        let validUrl = myInput.value;
+        const loadData = () => {
+            const url = `https://api.shrtco.de/v2/shorten?url=${validUrl}`;
+
+            console.log(url);
+            if (validUrl != '') {
+                fetch(url)
+                    .then(res => res.json())
+                    .then(data => displayShortUrl(data.result))
+                    .catch((error) => {
+                        console.log(error);
+                        alert("Something Wrong! Please Input Valid URL. Thank You.")
+                    })
+            } else {
+                alert("Something Wrong! Please Input Valid URL. Thank You.")
+            }
+
         }
 
-    }
 
-    const displayShortUrl = (getResult) => {
-        console.log(getResult.short_link3);
-        getShortener.innerHTML = `
+        const displayShortUrl = (getResult) => {
+            // start spinner 
+            toggleSpinner(true);
+
+            console.log(getResult.short_link3);
+            getShortener.innerHTML = `
         <a target="_blank" id="show-link-js"></a>
         `;
-        const getShowLinkJs = document.getElementById('show-link-js');
-        getShowLinkJs.innerText = getResult.short_link3;
-        getShowLinkJs.href = `https://${getResult.short_link3}`;
+            const getShowLinkJs = document.getElementById('show-link-js');
+            getShowLinkJs.innerText = getResult.short_link3;
+            getShowLinkJs.href = `https://${getResult.short_link3}`;
 
-        copyLinkBtn.addEventListener('click', function() {
-            const copyShowLink = getShowLinkJs.innerText;
-            navigator.clipboard.writeText(copyShowLink);
-            copyLinkBtn.innerText = "Copied";
+            // stop spinner 
+            toggleSpinner(false);
+
+            copyLinkBtn.addEventListener('click', function() {
+                const copyShowLink = getShowLinkJs.innerText;
+                navigator.clipboard.writeText(copyShowLink);
+                copyLinkBtn.innerText = "Copied";
+            })
+
+        }
+
+
+
+        qrCodeBtn.addEventListener('click', function() {
+            // start spinner 
+            toggleSpinner(true);
+            const loadQrCodeData = () => {
+
+                let validQrUrl = myInput.value;
+                const qrUrl = ` https://api.qrserver.com/v1/create-qr-code/?size=120x120&data=${validQrUrl}`
+                console.log(qrUrl);
+                qrCodeShow.innerHTML = `
+            <img src=${qrUrl} alt="QR CODE" class="pt-5">
+            `;
+            }
+            loadQrCodeData();
+            // stop spinner 
+            toggleSpinner(false);
         })
 
-    }
-
-    qrCodeBtn.addEventListener('click', function() {
-        const loadQrCodeData = () => {
-            let validQrUrl = myInput.value;
-            const qrUrl = ` https://api.qrserver.com/v1/create-qr-code/?size=120x120&data=${validQrUrl}`
-            console.log(qrUrl);
-            qrCodeShow.innerHTML = `
-            <img src=${qrUrl} alt="QR CODE" class="pt-5">
-            `
-        }
-        loadQrCodeData();
+        loadData();
 
     })
-    loadData();
-
-
-})
+    //loading
+const toggleSpinner = isLoading => {
+    const loadingSection = document.getElementById('spinner');
+    if (isLoading) {
+        loadingSection.classList.remove('hidden');
+    } else {
+        loadingSection.classList.add('hidden');
+    }
+}
